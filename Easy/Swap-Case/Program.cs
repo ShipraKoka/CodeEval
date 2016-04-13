166fe29d79437b1b0b7f3f6400fb8fdff928c08e
Shipra Koka <shipra.koka@gmail.com>
2016-04-13 16:12:35 -0700

    initial commit

---
+++ Easy/Swap-Case/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using System.IO;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		using (StreamReader reader = File.OpenText(args[0]))
+		while (!reader.EndOfStream)
+		{
+			// reads each line
+			string line = reader.ReadLine();
+			if (null == line)
+				continue;
+			string newLine = (swapCase(line));
+			Console.WriteLine(newLine);
+		}
+	}
+
+	static string swapCase(string x)
+	{
+		StringBuilder xLine = new StringBuilder();
+		for (int i = 0; i < x.Length; i++)
+		{
+			if (char.IsLetter(x[i]) && char.IsUpper(x[i]))
+				xLine.Append(char.ToLower(x[i]));
+			else if (char.IsLetter(x[i]) && char.IsLower(x[i]))
+				xLine.Append(char.ToUpper(x[i]));
+			else
+				xLine.Append(x[i]);
+		}
+		return xLine.ToString();
+	}
+}
