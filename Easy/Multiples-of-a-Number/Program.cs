5d2f480a2f2c2ca4f7a34ca89773e092e0ce2d45
Shipra Koka <shipra.koka@gmail.com>
2016-04-18 21:12:38 -0700

    initial commit

---
+++ Easy/Multiples-of-a-Number/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (StreamReader reader = File.OpenText(args[0]))
+        while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                if (null == line)
+                    continue;
+				
+                string[] numbers = line.Split(',');
+                int x = int.Parse(numbers[0]);
+                int n = int.Parse(numbers[1].TrimStart());
+				
+                int result = 0;
+                int i = 1;
+				
+                while (result < x)
+                {
+                    result = n * i;
+                    i++;
+                }
+				
+                Console.WriteLine(result.ToString());
+            }
+        
+    }
+
+}
