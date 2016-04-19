57d6df15766464f6a8b499de59c995eba6d3b678
Shipra Koka <shipra.koka@gmail.com>
2016-04-18 21:48:43 -0700

    initial commit

---
+++ Easy/Sum-of-Digits/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (StreamReader reader = File.OpenText(args[0]))
+        while (!reader.EndOfStream)
+		{
+			string line = reader.ReadLine();
+			if (null == line)
+				continue;
+
+			string digits = line.Trim().Replace("\n", " ");
+
+			int sum = 0;
+			foreach (var d in digits)
+			{
+				sum += (int)Char.GetNumericValue(d);
+			}
+			
+			Console.WriteLine(sum);
+		}
+    }
+}
