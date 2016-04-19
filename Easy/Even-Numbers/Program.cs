13416e5bdceb97a4232aed7b22687ad1c1226004
Shipra Koka <shipra.koka@gmail.com>
2016-04-19 14:20:13 -0700

    initial commit

---
+++ Easy/Even-Numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (StreamReader reader = File.OpenText(args[0]))
+        while (!reader.EndOfStream)
+        {
+            string line = reader.ReadLine();
+            if (null == line)
+                continue;
+
+            int x = int.Parse(line);
+
+            if (x % 2 == 0)
+                Console.WriteLine("1");
+            else
+                Console.WriteLine("0");
+        }
+    }
+}
