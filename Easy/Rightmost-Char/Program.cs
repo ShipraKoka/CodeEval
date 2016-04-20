fa26c77c6cabba1a1eac2286b04f05ba8f4bd859
Shipra Koka <shipra.koka@gmail.com>
2016-04-19 20:01:35 -0700

    initial commit

---
+++ Easy/Rightmost-Char/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (StreamReader reader = File.OpenText(args[0]))
+        while (!reader.EndOfStream)
+        {
+            string line = reader.ReadLine();
+            if (null == line)
+                continue;
+
+                string[] temp = line.Split(',');
+                string phrase = temp[0];
+                char t = char.Parse(temp[1]);
+
+                int pos = 0;
+                for (int i = 0; i < phrase.Length; i++)
+                {
+                    if (t == phrase[i])
+                        pos = i;
+                }
+                
+                if (pos != 0)
+                    Console.WriteLine(pos);
+                else
+                    Console.WriteLine("-1");
+        }
+    }
+}
