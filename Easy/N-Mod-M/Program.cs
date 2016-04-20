32a0ed1066c8f03f9238fb50d3ef1c5263b3a7c3
Shipra Koka <shipra.koka@gmail.com>
2016-04-19 20:16:49 -0700

    initial commit

---
+++ Easy/N-Mod-M/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (StreamReader reader = File.OpenText(args[0]))
+        while (!reader.EndOfStream)
+        {
+            string line = reader.ReadLine();
+            if (null == line)
+                continue;
+
+                string[] numbers = line.Split(',');
+                int N = int.Parse(numbers[0]);
+                int M = int.Parse(numbers[1]);
+
+                int factor = N / M;
+                int remainder = N - (factor * M);
+                Console.WriteLine(remainder);
+        }
+    }
+}
