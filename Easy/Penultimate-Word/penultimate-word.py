20e58498c9b9f4b5f491d95f97e64922a809cf17
Shipra Koka <shipra.koka@gmail.com>
2016-05-09 13:52:47 -0700

    remove strip method

--- Easy/Penultimate-Word/penultimate-word.py
+++ Easy/Penultimate-Word/penultimate-word.py
@@ -2,5 +2,5 @@ import sys
 
 with open(sys.argv[1], 'r') as test_cases:
     for test in test_cases:
-        line = test.strip("\n").split(" ")
+        line = test.split(" ")
         print(line[-2])
