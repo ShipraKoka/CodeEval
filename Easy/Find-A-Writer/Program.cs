212379e062c39af1ee46b4474ee76e45b2b47a12
Shipra Koka <shipra.koka@gmail.com>
2016-05-25 16:46:08 -0700

    initial commit

---
+++ Easy/Find-A-Writer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (StreamReader reader = File.OpenText(args[0]))
+        while (!reader.EndOfStream)
+        {
+            string line = reader.ReadLine();
+            if (null == line)
+                continue;
+
+                string writer = "";
+                string[] writers = line.Split('|');
+                string letters = writers[0];
+                string numbers = writers[1].TrimStart();
+                string[] key = numbers.Split(' ');
+
+                foreach (string item in key)
+                {
+                    writer += letters[int.Parse(item) - 1];
+                }
+
+                Console.WriteLine(writer);
+        }
+    }
+}
